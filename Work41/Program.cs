8a5a4e98533e3654b1dde43604fb7aecab6cb44d
KrisanovaOM <fentifluyshka@gmail.com>
2022-10-09 17:01:28 +0300

    Initial commit

---
+++ Work41/Program.cs
@@ -0,0 +1,32 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+string InputNumber()
+{
+  Console.WriteLine($"Введите числа через запятую: ");
+  return Console.ReadLine();
+}
+int[] ArrayNumber(string value)
+{  
+  string[] arrayString = value.Split(",").Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+  int[] arrayInt = new int[arrayString.Length];
+
+  for (int i = 0; i < arrayString.Length; i++)
+  {
+    arrayInt[i] = Convert.ToInt32(arrayString[i]);
+  }
+  return arrayInt;
+}
+int CountPositivNumber(int[] array)
+{
+  int count = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i] > 0)
+    {
+      count++;
+    }
+  }
+  return count;
+}
+
+Console.WriteLine($"Пользователь ввел чисел больше 0: {CountPositivNumber(ArrayNumber(InputNumber()))}");
---
+++ Work43/Program.cs
@@ -0,0 +1,48 @@
+﻿//  Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем. (b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5))
+
+double InputNumber(string name)
+{
+  Console.WriteLine($"Введите число {name}: ");
+  return Convert.ToDouble(Console.ReadLine());
+}
+void PrintArray(double[] array)
+{
+  Console.Write("(");
+  for (int i = 0; i < array.Length; i++)
+  {
+    Console.Write(array[i] +  (i != array.Length-1 ? ", " : ")"));
+  }
+  Console.WriteLine();
+}
+void IntersectionPoint(double b1, double k1, double b2, double k2)
+{
+  double x;  
+  double y;
+  if (k1 != k2)
+  {
+    x = (b2 - b1) / (k1 - k2);
+    y = k1 * x + b1;
+    double[] coordinate = new double[2];
+    for (int i = 0; i < coordinate.Length; i++)
+    {
+      if (i == 0)
+      {
+        coordinate[i] = x;
+      }
+      coordinate[i] = y;
+    }
+    PrintArray(coordinate);
+  }
+  else
+  {
+    Console.WriteLine("Точек пересечеия нет, прямые параллельны");
+  }
+}
+
+double b1 = InputNumber("b1");
+double k1 = InputNumber("k1");
+double b2 = InputNumber("b2");
+double k2 = InputNumber("k2");
+
+Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2}");
+IntersectionPoint(b1, k1, b2, k2);
